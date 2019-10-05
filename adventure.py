7e6affd06c1a262d775d0436b8cd5f0802669151
Tim Golden <mail@timgolden.me.uk>
2019-10-05 16:06:15 +0100

    Version worked on my Peter & Tommy

--- adventure.py
+++ adventure.py
@@ -1,47 +1,65 @@
-class State:
-    pass
-state = State()
+# Write your code here :-)
+from pprint import pprint
+rooms = {
+    (0, 0): "The hallway",
+    (1, 1): "The Living Room",
+    (0, 1): "The Kitchen",
+    (0, -1) : "The garden"
+}
+pprint(rooms)
 
-class Room:
-    def __init__(self, name):
-        self.name = self.description = name
-        self.exits = {}
-        self.items = []
+directions = {
+    "north": (0, +1),
+    "south": (0, -1),
+    "east": (+1,0),
+    "west": (-1,0)
+}
 
-class Item:
-    def __init__(self, name):
-        self.name = self.description = name
+current_coord = (0, 0)
 
-hallway = Room("The hallway")
-living_room = Room("The living room")
-garden = Room("The garden")
-kitchen = Room("The kitchen")
+allowed_verbs = ["go"]
+allowed_nouns = ["north", "south", "east" "west"]
 
-ball = Item("A ball")
-sword = Item("A sword")
-
-hallway.exits['E'] = living_room
-living_room.exits['W'] = hallway
-
-hallway.exits['W'] = kitchen
-kitchen.exits['E'] = hallway
-
-kitchen.exits['N'] = garden
-garden.exits['S'] = kitchen
-
-state.location = hallway
-state.items = []
+def check_exits(current_coord):
+    x,y = current_coord
+    possible_direction = directions[direction]
+    if possible_direction in rooms:
+        print("You can go to :", possible_direction)
 
 while True:
-    print("You are in", state.location.name)
-    instruction = input("What now? ")
-    verb, noun = instruction.upper().split()
+    room_name = rooms[current_coord]
+    print("You are in", room_name)
+    print("Type 'Go' followed a direction")
 
-    if verb == "GO":
-        direction = noun[0]
-        if direction in state.location.exits:
-            state.location = state.location.exits[direction]
-        else:
-            print("There is no exit to the", noun)
+    command = input("What next? ").lower()
+    command1=command.split()
+    if len(command1) not in (1, 2):
+        print("I can't handle this; my brain hurts")
+        continue
+
+    verb = command1[0]
+    print(verb)
+    if len(command1) == 2:
+        noun = command1[1]
     else:
-        print("I don't know how to", verb)
+        noun = None
+    print(noun)
+
+    if verb not in allowed_verbs:
+        print("I don't know what", verb ,"is")
+
+    if noun not in allowed_nouns:
+        print("I don't know what", noun ,"is")
+
+    if verb == 'go':
+        noun_coordinates=directions[noun]
+        x,y = current_coord
+        dx, dy = noun_coordinates
+
+        new_coord= ((x+dx),(y+dy))
+        print(new_coord)
+        if new_coord not in rooms:
+            print("You can't go this way")
+        else:
+            current_coord=new_coord
+
