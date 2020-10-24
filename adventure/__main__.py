20b31fd45e0b0a43068967c1f25ff29f289ec9eb
Tim Golden <mail@timgolden.me.uk>
2020-10-24 11:13:10 +0100

    Get a prototype multi-ui working (without any actual ui)

--- adventure/__main__.py
+++ adventure/__main__.py
@@ -1,30 +1,54 @@
 import os, sys
+import importlib
 import pathlib
 from pprint import pprint
 
-from .lib import adventurelib
+import jsonpickle
 
-def main(name):
-    initial_filepath = pathlib.Path(f"{name}.xlsx")
-    saved_filepath = pathlib.Path(f"{name}.saved")
+from .lib import adventurelib, base
 
-    if saved_filepath.exists():
-        adventure = adventurelib.adventure_from_pickle(saved_filepath)
-    elif initial_filepath.exists():
-        adventure = adventurelib.Adventure(name)
-        adventure.load_initial_game_from_spreadsheet(initial_filepath)
-    else:
-        raise RuntimeError("Can't find either a saved game or a spreadsheet")
+class Runner(base.Base):
 
-    print("Rooms:")
-    for name, room in adventure.rooms.items():
-        print(name)
-        for direction, room in room.exits.items():
-            print("  ", direction, "=>", room)
-    print("Item:")
-    pprint(adventure.inventory)
+    def __init__(self, player_name, adventure_name, ui_name):
+        super().__init__("runner")
+        self.player_name = player_name
+        self.adventure_name = adventure_name
+        self.ui_name = ui_name
 
-    adventure.save_game(saved_filepath)
+    def restart_game(self):
+        xlsx_filepath = pathlib.Path(f"{self.adventure_name}.xlsx")
+        adventure = adventurelib.Adventure(self.adventure_name, self.player_name)
+        adventure.load_initial_game_from_spreadsheet(xlsx_filepath)
+        return adventure
+
+    def load_game(self, filepath):
+        with open(filepath, encoding="utf-8") as f:
+            return jsonpickle.decode(f.read())
+
+    def save_game(self, adventure, filepath):
+        with open(filepath, "w", encoding="utf-8") as f:
+            f.write(jsonpickle.encode(adventure))
+
+    def run(self):
+        saved_filepath = pathlib.Path(f"{self.adventure_name}.saved")
+        if saved_filepath.exists():
+            adventure = self.load_game(saved_filepath)
+        else:
+            adventure = self.restart_game()
+
+        ui = importlib.import_module(".ui.%s" % self.ui_name, __package__)
+
+        try:
+            ui.run(adventure)
+        finally:
+            self.save_game(adventure, saved_filepath)
+
+def main(adventure_name=None, player_name=None, ui_name="text"):
+    if not adventure_name:
+        adventure_name = input("Adventure: ")
+    if not player_name:
+        player_name = input("Player: ")
+    Runner(player_name, adventure_name, ui_name).run()
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
--- adventure/lib/adventurelib.py
+++ adventure/lib/adventurelib.py
@@ -1,10 +1,13 @@
 import os, sys
 import collections
 import json
-import pickle
+import pathlib
 from pprint import pprint
 
+import jsonpickle
 import openpyxl
+
+from . import base
 
 BORDER_OFFSETS = [
     ("top", (0, -1), "north"),
@@ -13,22 +16,7 @@ BORDER_OFFSETS = [
     ("right", (+1, 0), "east")
 ]
 
-class Base:
-
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return "<%s: %s>" % (self.__class__.__name__, self.name)
-
-    @classmethod
-    def from_namedtuple(cls, info):
-        return cls(*info)
-
-class Item(Base):
+class Item(base.DataBase):
 
     def __init__(self, name, weight, description, initial_location, initial_sublocation):
         super().__init__(name)
@@ -37,16 +25,7 @@ class Item(Base):
         self.initial_location = initial_location
         self.initial_sublocation = initial_sublocation
 
-    def as_dict(self):
-        return dict(
-            weight=self.weight,
-            description=self.description,
-            initial_location=self.initial_location,
-            initial_sublocation=self.initial_sublocation
-        )
-
-
-class Room(Base):
+class Room(base.DataBase):
 
     def __init__(self, name, description, is_initial):
         super().__init__(name)
@@ -54,13 +33,7 @@ class Room(Base):
         self.exits = dict()
         self.is_initial = (is_initial == "Yes")
 
-    def as_dict(self):
-        return dict(
-            description=self.description,
-            exits=[(direction, room.name) for (direction, room) in self.exits.items()]
-        )
-
-class Actor(Base):
+class Actor(base.DataBase):
 
     def __init__(self, name, score=10):
         super().__init__(name)
@@ -71,29 +44,18 @@ class Actor(Base):
     def move_to(self, new_location):
         self.location = new_location
 
-def adventure_from_pickle(filepath):
-    with open(filepath, "rb") as f:
-        return pickle.load(f)
-
-class Adventure(object):
+class Adventure(base.Base):
 
     def __init__(self, name, player_name):
         """Set up an adventure from details in a spreadsheet
         """
-        self.name = name
+        super().__init__(name)
         self.player = Actor(player_name)
 
-    def load_from_saved_game(self, filepath):
-        raise NotImplementedError
-
-    def save_game(self, filepath):
-        with open(filepath, "wb") as f:
-            pickle.dump(self, f)
-
-    def load_initial_game_from_spreadsheet(self, filepath):
+    def load_initial_game_from_spreadsheet(self, filepath=None):
         """Load the rooms, layout and items from a spreadsheet
         """
-        wb = openpyxl.load_workbook(filepath)
+        wb = openpyxl.load_workbook(filepath or "%s.xlsx" % self.name)
         try:
             self.rooms = self.get_rooms(wb)
             layout = self.get_layout(wb)
@@ -104,7 +66,7 @@ class Adventure(object):
 
             for room in self.rooms.values():
                 if room.is_initial:
-                    self.actor.move_to(room)
+                    self.player.move_to(room)
         finally:
             wb.close()
 
@@ -154,4 +116,11 @@ class Adventure(object):
         """Return a dictionary mapping room names to details"""
         return dict((r.name, r) for r in self.sheet_to_namedtuples(wb, "Rooms", Room))
 
-
+    def run(self):
+        print("Rooms:")
+        for name, room in adventure.rooms.items():
+            print(name)
+            for direction, room in room.exits.items():
+                print("  ", direction, "=>", room)
+        print("Item:")
+        pprint(adventure.inventory)
